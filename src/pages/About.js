0e90a50f4c1f9cbaf4763202cf841502d0295a28
ru0618 <riley60530@gmail.com>
2023-11-17 04:50:14 +0800

    edit About

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,9 @@ import React from "react";
 
 const About = () => {
   return (
-    <div style={{ minHeight: "100vh" }}>
-      About
+    <div style={{ display: "flex", justifyContent: "center" }} >
+      <img src="hi.jpg" style={{ width: "40%" }} />
+      <p style={{ textAlign: "center", padding: "2rem", display: "flex", alignItems: "center" }}>串接 Pexels API 製作相片圖庫，僅作練習之用</p>
     </div>
   );
 };
