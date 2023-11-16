a256afc6fc021a20e244d038ffb96beb50ff46fd
ru0618 <riley60530@gmail.com>
2023-11-17 04:16:29 +0800

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>GuanRu Chen 2023</div>
+  )
+}
+
+export default Footer
---
+++ src/pages/Homepage.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Search from "../components/Search";
+import Picture from "../components/Picture";
+
+const Homepage = () => {
+  let [input, setInput] = useState("");
+  let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
+  const auth = "ojjVeGtbGnmyMplS9jCtqsdsJlnyPEZ0Gt3Tl89qCzF0Wqh18MjOvKBb";
+  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
+
+  const search = async (url) => {
+    let result = await axios.get(url, {
+      headers: { Authorization: auth },
+    });
+    console.log(result);
+    setData(result.data.photos);
+    setCurrentSearch(input);
+  };
+
+  // Closure
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${
+        page + 1
+      }`;
+    }
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setData(data.concat(result.data.photos));
+  };
+
+  // 讓Homepage一被渲染（一打開頁面）時就產生圖片，而不是按search才出現圖片
+  useEffect(() => {
+    search(initialURL);
+  }, []);
+
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
+      <div className="pictures">
+        {data &&
+          data.map((d) => {
+            return <Picture data={d} />;
+          })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
